007280c3f62f76a3166d82feeb6f1b458e38dd67
joseerodrigues <joseerodrigues@gmail.com>
2017-05-09 11:55:12 +0100

    added StringResultSetMapper and Mappers util

---
+++ src/main/java/com/cave/utils/dbutil/mapper/Mappers.java
@@ -0,0 +1,34 @@
+package com.cave.utils.dbutil.mapper;
+
+import com.cave.utils.dbutil.ResultSetMapper;
+
+public final class Mappers {
+
+    public static ResultSetMapper<ResultSetMap> mapMapper(){
+        return new MapResultSetMapper();
+    }
+
+    public static ResultSetMapper<Integer> intMapper(String colName){
+        return new BasicResultSetMapper<Integer>(colName);
+    }
+
+    public static ResultSetMapper<Integer> intMapper(int colIndex){
+        return new BasicResultSetMapper<Integer>(colIndex);
+    }
+
+    public static ResultSetMapper<String> stringMapper(String colName){
+        return new StringResultSetMapper(colName, false);
+    }
+
+    public static ResultSetMapper<String> stringMapper(int colIndex){
+        return new StringResultSetMapper(colIndex, false);
+    }
+
+    public static ResultSetMapper<String> stringMapper(String colName, boolean trim){
+        return new StringResultSetMapper(colName, trim);
+    }
+
+    public static ResultSetMapper<String> stringMapper(int colIndex, boolean trim){
+        return new StringResultSetMapper(colIndex, false);
+    }
+}
